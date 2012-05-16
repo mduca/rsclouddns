5054b28b6fc983fbda634a8c32e66c4be3940f15
mduca <michael@mandu.ca>
2012-05-16 01:34:55 -0500

    moved example_config

--- example_config.rb
+++
@@ -1,7 +0,0 @@
-def config option
-  #Add configs as needed
-  config = {"USER" => "EMANRESU", "PASS" => "ch4ng3_m3", "API" => "api_key_here_2394lk2j34ljkg2lkj34"}
-  if config.has_key? option
-    config[option] 
-  end
-end
