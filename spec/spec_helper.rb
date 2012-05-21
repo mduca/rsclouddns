5b9096c1d9f44231c7c1b5990a071a8a737a0bb2
mduca <michael@mandu.ca>
2012-05-21 14:18:58 -0500

    added regex redaction to cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,13 +15,16 @@ VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = true
   c.cassette_library_dir = 'spec/fixtures/rsclouddns_cassettes'
   c.hook_into :webmock
-  c.filter_sensitive_data('<REDACTED>') {"#{config('USERNAME')}"} 
-  c.filter_sensitive_data('<REDACTED>') {"#{config('PASSWORD')}"} 
-  c.filter_sensitive_data('<REDACTED>') {"#{config('API_KEY')}"} 
-  c.filter_sensitive_data('<TENANTID>') {"#{config('TENANTID')}"} 
+  c.filter_sensitive_data('[REDACTED]') {"#{config('USERNAME')}"} 
+  c.filter_sensitive_data('[REDACTED]') {"#{config('PASSWORD')}"} 
+  c.filter_sensitive_data('[REDACTED]') {"#{config('API_KEY')}"} 
+  c.filter_sensitive_data('[TENANTID]') {"#{config('TENANTID')}"} 
 
   c.before_record do |i|
-    #Redacted until proper regex are created
-    i.response.body.gsub!(/.*/,'<REDACTED>')
+  #Redacted until proper regex are created
+    i.response.body.gsub!(/[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+/, '[REDACTED]')
   end
+  
+  #   Regex
+  #   /^[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+/
 end
